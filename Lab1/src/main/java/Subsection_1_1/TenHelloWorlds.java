3471a0eae22a7d10c08f21a44989c4c872a874c6
QuanOnThePC <98805733+quan11304@users.noreply.github.com>
2023-12-17 21:50:19 +0700

    L1 - Subsection 1.1 finished

--- Lab1/src/main/java/Subsection_1_1/TenHelloWorlds.java
+++ Lab1/src/main/java/Subsection_1_1/TenHelloWorlds.java
@@ -1,4 +1,4 @@
-public class Exercise_1 {
+public class TenHelloWorlds {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
--- Lab1/src/main/java/Subsection_1_1/UseThree.java
+++ Lab1/src/main/java/Subsection_1_1/UseThree.java
@@ -1,4 +1,4 @@
-public class Exercise_5 {
+public class UseThree {
     public static void main(String[] args) {
         System.out.printf("Hi %s, %s and %s",args[2],args[1],args[0]);
     }
